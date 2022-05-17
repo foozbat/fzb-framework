ae61967b7cc360232b23026a2867684512e956f6
Aaron <aaron@foozbat.net>
2022-05-17 15:07:14 +0000

    test

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 	date:         6/18/2019
 	description:  This is the main entry point for the application.  All common initialization is done here and this script functions as the main controller.
 */
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // app specific initialization
